d0870b8930f6771caf93dab3889682e439e91fc5
terrastation2 <terrastation2@proton.me>
2025-11-25 18:39:37 -0300

    Update callsat.sol

--- callsat.sol
+++ callsat.sol
@@ -1,4 +1,4 @@
-  // CALLSAT by TS-2 v2.1 - Shows how many times this contract was called - 22-11-2025
+  // CALLSAT by TS-2 v2.1 - Shows how many times this contract was called - 25-11-2025
 
 pragma solidity ^0.8.9;
 
